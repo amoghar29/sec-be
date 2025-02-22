bcc0da69c98ff6252d0802f9596c33ed8f00f352
amogha r <amoghar23@gmail.com>
2025-02-22 23:47:37 +0530

    singout

--- src/controllers/auth/signout.ts
+++ src/controllers/auth/signout.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
 export const signout = async (req: Request, res: Response): Promise<void> => {
-    res.clearCookie("token");
+    res.clearCookie("token",{path:"/"});
     res.status(200).json({ message: "Logged out successfully" });
   };
